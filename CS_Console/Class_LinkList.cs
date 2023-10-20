fe0a33f46ec73bdea167df85b866df53f707248b
Robinsssson <18656133516@qq.com>
2023-10-20 11:46:00 +0800

    new file:   CS_Console.sln

---
+++ CS_Console/Class_LinkList.cs
@@ -0,0 +1,83 @@
+﻿namespace CS_Console
+{
+    interface IListOperate<T>
+    {
+        public void PushBack(T val);
+        public T PopBack();
+        public T GetBack(int index);
+        public T GetValue(int index);
+        public void SetValue(int index, T val);
+        public void Clear();
+        public bool Contains(T item);
+        public ref T GetItem(int index);
+
+    }
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> next {  get; set; }
+        public Node() { Value = default; next = null; }
+
+        public Node<T> GetNext() { return  next; }
+    }
+    public class LinkList<T> : IListOperate<T>
+    {
+        private readonly Node<T> L;
+        public LinkList()
+        {
+            L = new Node<T>();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetBack(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ref T GetItem(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetValue(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T PopBack()
+        {
+            if (L.next == null)
+                return default;
+            Node<T> node = L.next;
+            while (node.next != null)
+            {
+                node = node.next;
+            }
+            T tmp = node.Value;
+            return tmp;
+        }
+
+        public void PushBack(T val)
+        {
+            Node<T> N = new()
+            {
+                Value = val
+            };
+            L.next = N;
+        }
+
+        public void SetValue(int index, T val)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CS_Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CS_Console;
+namespace HelloWorldApplication
+{
+    class HelloWorld
+    {
+        static void Main()
+        {
+            LinkList<int> ints = new();
+            ints.PushBack(12);
+            ints.PushBack(13);
+            Console.WriteLine(ints.PopBack());
+            Console.ReadKey();
+        }
+    }
+}
